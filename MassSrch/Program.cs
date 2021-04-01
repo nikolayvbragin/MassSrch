f517ad543579405aaf57cb3ff576d9b17458c605
nik_bragin@mail.ru <nik_bragin@mail.ru>
2021-04-01 16:26:40 +0700

    1-st

--- MassSrch/Program.cs
+++ MassSrch/Program.cs
@@ -4,9 +4,43 @@ namespace MassSrch
 {
     class Program
     {
+        static int TryParseInput()
+        {
+            int OutVal;
+            while (!int.TryParse(Console.ReadLine(), out OutVal))
+            {
+                Console.WriteLine("Введенное значение не является целым числом, повторите ввод:");
+            }
+            return OutVal;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите количество элементов массива:");
+            int ArrLen = TryParseInput();
+            Console.WriteLine($"Заданная длина массива: {ArrLen} элемента(ов)");
+            int[] MyArr = new int[ArrLen];
+            Console.WriteLine("Заполнение массива");
+            for (int i = 0; i < ArrLen; i++)
+            {
+                Console.WriteLine($"Введите элемент массива № {i + 1}");
+                MyArr[i] = TryParseInput();
+            }
+
+            Console.WriteLine("Вывод содержимого массива:");
+            foreach (int Val in MyArr)
+            {
+                Console.Write($"{Val} "); 
+            }
+            Console.WriteLine();
+            int largestVal = 0;
+            int secondLargestVal = 0;
+            foreach (var Val in MyArr) if (Val > largestVal) largestVal = Val;
+            Console.WriteLine($"Наибольшее значение: {largestVal}");
+            foreach (var Val in MyArr)
+            {
+                if (Val > secondLargestVal && Val < largestVal) secondLargestVal = Val;
+            }
+            Console.WriteLine($"Второе наибольшее значение: {secondLargestVal}");
         }
     }
 }
